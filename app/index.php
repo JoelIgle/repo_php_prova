458d15888e53895ecf14c8ec55bef49ec2ee8381
JoelIgle <joeliglesias@iesebre.com>
2023-11-17 15:50:30 +0100

    refactoritzacio de codi

--- app/index.php
+++ app/index.php
@@ -1,26 +1,13 @@
 <?php
 
-require  'helpers.php';
-require 'Task.php';
 require 'config.php';
-//$task = new  Task(1,'comprar pa','a la panaderia',0);
+require 'helpers.php';
+require 'Task.php';
 
-
-$user = $config['database']['user'];
-$pass = $config['database']['password'];
-$type = $config['database']['databasetype'];
-$host = $config['database']['host'];
-$name = $config['database']['name'];
-$dsn = "$type:host=$host;dbname=$name";
-
-
-try {
-    $dbh = new PDO($dsn, $user, $pass);
-}catch (\Exception $e){
-    echo "Error de connexió a la base de dades!: ";
-}
+$dbh = connectDB($config);
 
 $statement = $dbh->prepare('SELECT * FROM tasks;');
+
 $statement->execute();
 
 $tasks = $statement->fetchAll(PDO::FETCH_OBJ);
